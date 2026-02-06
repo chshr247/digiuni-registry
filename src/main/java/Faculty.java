8c1b526b4d7e40fae7f5766563aa85d0aa818db7
Slava Mokliak <slava.mokliak@gmail.com>
2026-02-06 17:09:16 +0200

    Added Faculty file

---
+++ src/main/java/Faculty.java
@@ -0,0 +1,81 @@
+import java.util.Objects;
+
+public class Faculty{
+    String id;
+    String fullName;
+    String shortName;
+    String dean;
+    String contact;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getDean() {
+        return dean;
+    }
+
+    public void setDean(String dean) {
+        this.dean = dean;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public Faculty(String id, String fullName, String shortName, String dean, String contact) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.dean = dean;
+        this.contact = contact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return Objects.equals(id, faculty.id) && Objects.equals(fullName, faculty.fullName) && Objects.equals(shortName, faculty.shortName) && Objects.equals(dean, faculty.dean) && Objects.equals(contact, faculty.contact);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, shortName, dean, contact);
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", shortName='" + shortName + '\'' +
+                ", dean='" + dean + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
+}
+
