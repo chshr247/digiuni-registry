50b14f1e8dc064d3fa9649cd8e81ab6bff679b15
Viacheslav Mokliak <slava.mokliak@gmail.com>
2026-02-07 16:46:45 +0200

    Some student changes

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,20 +1,11 @@
 import java.util.Objects;
 
 public class Student extends Person{
-    String studentId;
     int grade;
     int group;
     int year;
     String formOfStudy;
     String status;
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
 
     public int getGrade() {
         return grade;
@@ -61,12 +52,12 @@ public class Student extends Person{
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Student student = (Student) o;
-        return grade == student.grade && group == student.group && year == student.year && Objects.equals(studentId, student.studentId) && Objects.equals(formOfStudy, student.formOfStudy) && Objects.equals(status, student.status);
+        return grade == student.grade && group == student.group && year == student.year && Objects.equals(formOfStudy, student.formOfStudy) && Objects.equals(status, student.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), studentId, grade, group, year, formOfStudy, status);
+        return Objects.hash(super.hashCode(), grade, group, year, formOfStudy, status);
     }
 
     @Override
@@ -82,13 +73,11 @@ public class Student extends Person{
                 ", year=" + year +
                 ", group=" + group +
                 ", grade=" + grade +
-                ", studentId='" + studentId + '\'' +
                 '}';
     }
 
-    public Student(String id, String fullName, String birthDate, String email, String phone, String studentId, int grade, int group, int year, String formOfStudy, String status) {
+    public Student(String id, String fullName, String birthDate, String email, String phone, int grade, int group, int year, String formOfStudy, String status) {
         super(id, fullName, birthDate, email, phone);
-        this.studentId = studentId;
         this.grade = grade;
         this.group = group;
         this.year = year;
