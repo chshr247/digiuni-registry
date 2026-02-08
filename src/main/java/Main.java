5be55c1f321c4d69321d1727a3d773753e694764
Viacheslav Mokliak <slava.mokliak@gmail.com>
2026-02-09 00:49:04 +0200

    Made menu for CRUD access

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        CRUD.create();
-        CRUD.showStudents();
-        CRUD.update();
-        CRUD.delete();
-        CRUD.showStudents();
+        boolean running = true;
 
+        while (running) {
+            System.out.println("""
+                    --- MENU ---
+                    1. Add Student
+                    2. List Students
+                    3. Update Student
+                    4. Delete Student
+                    0. Exit
+                    """);
+
+            int choice = CRUD.intInRange("Your choice: ", 0, 4);
+
+            switch (choice) {
+                case 1 -> CRUD.create();
+                case 2 -> CRUD.showStudents();
+                case 3 -> CRUD.update();
+                case 4 -> CRUD.delete();
+                case 0 -> running = false;
+            }
+        }
+        System.out.println("Program finished.");
     }
 }
